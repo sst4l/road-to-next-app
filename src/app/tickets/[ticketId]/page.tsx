c004a035bc9107d35ee8f795f00cdac6f4416ff9
sst4l <selfstudytech@gmail.com>
2025-02-01 07:35:35 -0600

    Display ticket contents or implement ticket not found

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,5 @@
+import { initialTickets } from "@/app/data";
+
 type TicketPageProps = {
     params: Promise<{
       ticketId: string;
@@ -6,8 +8,19 @@ type TicketPageProps = {
   
   const TicketPage = async ({ params }: TicketPageProps) => {
     const { ticketId } = await params;
-  
-    return <h2 className="text-lg">TicketPage: {ticketId}</h2>;
+
+    const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+
+    if (!ticket) {
+      return <div>Ticket not found</div>;
+    }
+
+    return (
+      <div>
+        <h2 className="text-lg">TicketPage: {ticketId}</h2>;
+        <p className="text-sm">{ticket.content}</p>
+      </div>
+    );
   };
   
   export default TicketPage;
